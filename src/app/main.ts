d8b44e99139cfe84e8000d271ffe75694bb2a924
k.vlasov <flockast@gmail.com>
2026-01-31 22:12:18 +0300

    updated main file

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,18 +1,13 @@
-import { createApp, nextTick } from 'vue'
+import { createApp } from 'vue'
 import { withProviders } from './providers'
-// import { fadeOutLoading, setErrorPage } from './helpers'
 import App from './App.vue'
 
 const bootstrap = async () => {
   try {
-    const { app, router }  = await withProviders(createApp(App))
+    const { app }  = await withProviders(createApp(App))
     app.mount('#app')
-    await router.isReady()
-    await nextTick()
-    await new Promise((resolve) => setTimeout(resolve, 300))
-    // await fadeOutLoading()
   } catch {
-    // setErrorPage()
+    console.error('Initialization error')
   }
 }
 
